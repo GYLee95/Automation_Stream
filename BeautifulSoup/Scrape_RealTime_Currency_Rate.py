020d217df8ac5b4c7ee473c4b34143a1da4296e5
GYLee95 <117147089+GYLee95@users.noreply.github.com>
2024-10-20 14:33:48 +0000

    Update 20 Oct 2024

---
+++ BeautifulSoup/Scrape_RealTime_Currency_Rate.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+default_from_rate = "USD"
+default_to_rate = "MYR"
+default_amount = 100
+
+
+
+def get_currency_rate(from_rate, to_rate, amount):
+  url = f"https://www.x-rates.com/calculator/?from={default_from_rate}&to={default_to_rate}&amount={default_amount}"
+  response = requests.get(url)
+  content = response.content
+  soup = BeautifulSoup(content, "html.parser")
+  rate = soup.find("span", class_="ccOutputRslt").get_text()
+  #rate = float(rate.replace(",", ""))
+  rate = float(rate.split(" ")[0])
+  return rate
+
+
+def main(run=False):
+  if run:
+    rate = get_currency_rate(default_from_rate,default_to_rate,default_amount)
+    print(rate)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, './Introduction')
 sys.path.insert(0, './Selenium')
 sys.path.insert(0, './Requests')
+sys.path.insert(0, './BeautifulSoup')
 
 import intro_main as proc_1
 import Scrape_Simple_Text_with_Selenium as proc_2
@@ -9,6 +10,7 @@ import Automate_Login as proc_3
 import Login_and_Scrape as proc_4
 import Access_titan22 as proc_5
 import Download_Stock_Data as proc_6
+import Scrape_RealTime_Currency_Rate as proc_7
 
 # --- Set run status
 proc_1_run = 0
@@ -16,7 +18,8 @@ proc_2_run = 0
 proc_3_run = 0
 proc_4_run = 0
 proc_5_run = 0
-proc_6_run = 1
+proc_6_run = 0
+proc_7_run = 1
 
 # --- Trigger main function
 proc_1.main(proc_1_run)
@@ -25,3 +28,4 @@ proc_3.main(proc_3_run)
 proc_4.main(proc_4_run)
 proc_5.main(proc_5_run)
 proc_6.main(proc_6_run)
+proc_7.main(proc_7_run)
